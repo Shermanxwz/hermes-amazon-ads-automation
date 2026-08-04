0358400405459bbfc3b1e65907ba4aa4d615a045
Shermanxwz <102407646+Shermanxwz@users.noreply.github.com>
2026-08-04 21:48:53 +0800

    Install approval-gated autopilot control

--- control-plane/amazon_ads_control/__init__.py
+++ control-plane/amazon_ads_control/__init__.py
@@ -4,7 +4,7 @@ from .closed_loop import install as _install_closed_loop
 from .closed_loop_fixes import install as _install_closed_loop_fixes
 from .strategy_hardening import install as _install_strategy_hardening
 
-__version__ = "3.1.0"
+__version__ = "3.2.0"
 
 _install_closed_loop()
 _install_closed_loop_fixes()
@@ -16,14 +16,16 @@ from .task_hardening import install as _install_task_hardening
 from .storage_maintenance import install as _install_storage_maintenance
 from .storage_alert_rollup import install as _install_storage_alert_rollup
 from .api_extension import install as _install_api_extension
+from .approval_gate import install as _install_approval_gate
 _install_report_evidence_hardening()
 _install_callback_hardening()
 _install_task_hardening()
 _install_storage_maintenance()
 _install_storage_alert_rollup()
 _install_api_extension()
+_install_approval_gate()
 
 del _install_closed_loop, _install_closed_loop_fixes, _install_strategy_hardening
 del _install_report_evidence_hardening, _install_callback_hardening
 del _install_task_hardening, _install_storage_maintenance, _install_storage_alert_rollup
-del _install_api_extension
+del _install_api_extension, _install_approval_gate
