986bc836fed5767a4bfc09f936acaaddd7970572
Shermanxwz <102407646+Shermanxwz@users.noreply.github.com>
2026-08-04 21:58:48 +0800

    Install Hermes compatibility policy

--- control-plane/amazon_ads_control/__init__.py
+++ control-plane/amazon_ads_control/__init__.py
@@ -17,6 +17,7 @@ from .storage_maintenance import install as _install_storage_maintenance
 from .storage_alert_rollup import install as _install_storage_alert_rollup
 from .api_extension import install as _install_api_extension
 from .approval_gate import install as _install_approval_gate
+from .hermes_compat import install as _install_hermes_compat
 from .hermes_lifecycle import install as _install_hermes_lifecycle
 _install_report_evidence_hardening()
 _install_callback_hardening()
@@ -25,9 +26,10 @@ _install_storage_maintenance()
 _install_storage_alert_rollup()
 _install_api_extension()
 _install_approval_gate()
+_install_hermes_compat()
 _install_hermes_lifecycle()
 
 del _install_closed_loop, _install_closed_loop_fixes, _install_strategy_hardening
 del _install_report_evidence_hardening, _install_callback_hardening
 del _install_task_hardening, _install_storage_maintenance, _install_storage_alert_rollup
-del _install_api_extension, _install_approval_gate, _install_hermes_lifecycle
+del _install_api_extension, _install_approval_gate, _install_hermes_compat, _install_hermes_lifecycle
