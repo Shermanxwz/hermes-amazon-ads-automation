7df9050166255bfe426d59798e115c4de22aea47
Shermanxwz <102407646+Shermanxwz@users.noreply.github.com>
2026-08-04 23:53:26 +0800

    Install write batch hardening at package startup

--- control-plane/amazon_ads_control/__init__.py
+++ control-plane/amazon_ads_control/__init__.py
@@ -20,6 +20,7 @@ from .approval_gate import install as _install_approval_gate
 from .approval_hardening import install as _install_approval_hardening
 from .structural_execution import install as _install_structural_execution
 from .structural_hardening import install as _install_structural_hardening
+from .write_batch_hardening import install as _install_write_batch_hardening
 from .hermes_compat import install as _install_hermes_compat
 from .hermes_lifecycle import install as _install_hermes_lifecycle
 _install_report_evidence_hardening()
@@ -32,6 +33,7 @@ _install_approval_gate()
 _install_approval_hardening()
 _install_structural_execution()
 _install_structural_hardening()
+_install_write_batch_hardening()
 _install_hermes_compat()
 _install_hermes_lifecycle()
 
@@ -40,4 +42,4 @@ del _install_report_evidence_hardening, _install_callback_hardening
 del _install_task_hardening, _install_storage_maintenance, _install_storage_alert_rollup
 del _install_api_extension, _install_approval_gate, _install_approval_hardening
 del _install_structural_execution, _install_structural_hardening
-del _install_hermes_compat, _install_hermes_lifecycle
+del _install_write_batch_hardening, _install_hermes_compat, _install_hermes_lifecycle
