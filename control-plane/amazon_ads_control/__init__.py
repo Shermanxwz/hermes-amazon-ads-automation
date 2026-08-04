d67e16079643ea675d41d760ce932c7a1e4011f0
Shermanxwz <102407646+Shermanxwz@users.noreply.github.com>
2026-08-04 22:23:11 +0800

    Install deterministic structural execution chain

--- control-plane/amazon_ads_control/__init__.py
+++ control-plane/amazon_ads_control/__init__.py
@@ -18,6 +18,7 @@ from .storage_alert_rollup import install as _install_storage_alert_rollup
 from .api_extension import install as _install_api_extension
 from .approval_gate import install as _install_approval_gate
 from .approval_hardening import install as _install_approval_hardening
+from .structural_execution import install as _install_structural_execution
 from .hermes_compat import install as _install_hermes_compat
 from .hermes_lifecycle import install as _install_hermes_lifecycle
 _install_report_evidence_hardening()
@@ -28,6 +29,7 @@ _install_storage_alert_rollup()
 _install_api_extension()
 _install_approval_gate()
 _install_approval_hardening()
+_install_structural_execution()
 _install_hermes_compat()
 _install_hermes_lifecycle()
 
@@ -35,4 +37,4 @@ del _install_closed_loop, _install_closed_loop_fixes, _install_strategy_hardenin
 del _install_report_evidence_hardening, _install_callback_hardening
 del _install_task_hardening, _install_storage_maintenance, _install_storage_alert_rollup
 del _install_api_extension, _install_approval_gate, _install_approval_hardening
-del _install_hermes_compat, _install_hermes_lifecycle
+del _install_structural_execution, _install_hermes_compat, _install_hermes_lifecycle
