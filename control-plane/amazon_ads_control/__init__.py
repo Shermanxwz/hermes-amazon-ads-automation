446de7257a96499e720c684237bdd3543a0dbe65
Shermanxwz <102407646+Shermanxwz@users.noreply.github.com>
2026-08-04 18:12:45 +0800

    Install autonomous storage maintenance

--- control-plane/amazon_ads_control/__init__.py
+++ control-plane/amazon_ads_control/__init__.py
@@ -4,7 +4,7 @@ from .closed_loop import install as _install_closed_loop
 from .closed_loop_fixes import install as _install_closed_loop_fixes
 from .strategy_hardening import install as _install_strategy_hardening
 
-__version__ = "3.0.0"
+__version__ = "3.1.0"
 
 _install_closed_loop()
 _install_closed_loop_fixes()
@@ -13,12 +13,14 @@ _install_strategy_hardening()
 from .report_evidence_hardening import install as _install_report_evidence_hardening
 from .callback_hardening import install as _install_callback_hardening
 from .task_hardening import install as _install_task_hardening
+from .storage_maintenance import install as _install_storage_maintenance
 from .api_extension import install as _install_api_extension
 _install_report_evidence_hardening()
 _install_callback_hardening()
 _install_task_hardening()
+_install_storage_maintenance()
 _install_api_extension()
 
 del _install_closed_loop, _install_closed_loop_fixes, _install_strategy_hardening
 del _install_report_evidence_hardening, _install_callback_hardening
-del _install_task_hardening, _install_api_extension
+del _install_task_hardening, _install_storage_maintenance, _install_api_extension
